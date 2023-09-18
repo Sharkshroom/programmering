6c919e4f6153b20d18ac9b8597caab8196c0cc8d
oliwer paffrath <oliwerpaffrath@gmail.com>
2023-09-18 13:35:51 +0200

    klar

--- 7040 in och utmatning/uppgift_1.py
+++ 7040 in och utmatning/uppgift_1.py
@@ -1,2 +1,14 @@
-namn = input("Vad heter du?")
-print("Hej", namn, "!")
+'''
+Skriv ett program som gör följande.
+
+Be användaren mata in sin favoritfärg.
+
+Be användaren att även mata in sitt namn.
+
+Gör en utskrift som blir ungefär så här.
+'''
+
+
+färg = input("skriv din farvorit färg")
+namn = input ("vad heter du?")
+print(namn, " din favoritfärg är", färg)
