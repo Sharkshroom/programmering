3c5fa11368d2d32dc7ad397fbe0e9cf371d2f234
oliwer paffrath <oliwerpaffrath@gmail.com>
2023-10-16 13:24:00 +0200

    klart

---
+++ 7040 in och utmatning/uppgift_2.py
@@ -0,0 +1,21 @@
+'''
+Skriv ett program som gör följande.
+Be användaren att mata in vad den heter.
+
+Svara ungefär så här.
+
+Rikard är ett fint namn.
+Fråga användaren vilken mat den gillar.
+
+Svara ungefär så här.
+
+Rikard kroppkakor är verkligen gott.
+Fråga användaren hur mycket pengar den har i fickan.
+
+Svara ungefär så här.
+
+Rikard du är rik.'''
+
+input,("hej vad heter du?")
+print,( namn, )
+input ("namn, ")
