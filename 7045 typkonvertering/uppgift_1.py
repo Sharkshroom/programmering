575ac64974231cae00c1ab8639a36b49f3a276f9
oliwer paffrath <oliwerpaffrath@gmail.com>
2023-10-26 10:50:00 +0200

    klar upgift 7045 1

---
+++ 7045 typkonvertering/uppgift_1.py
@@ -0,0 +1,8 @@
+bredd = float(input ("ange rektangelns bredd:"))
+höjd = float (input("ange rektangelns höjd:"))
+area = bredd * höjd / 2
+
+
+print("area:", area)
+print("bredd", bredd )
+print("höjd", höjd)
